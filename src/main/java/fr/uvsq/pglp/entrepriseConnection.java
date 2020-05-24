0f04fb92b2e8554c9af698a324b1d25b47d7f197
eloifabius <fabius1995>
2020-05-24 04:09:53 +0200

    implémentation des classes personnel

---
+++ src/main/java/fr/uvsq/pglp/entrepriseConnection.java
@@ -0,0 +1,37 @@
+package fr.uvsq.pglp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class entrepriseConnection {
+	
+	private static String JDBC_URL="jdbc:derby:PersoDB;create=true";
+	private static final String DRIVER="org.apache.derby.jdbc.EmbeddedDriver";
+	
+	public static Connection getInstanceConnexion()
+	{
+		try {
+			
+				Class.forName(DRIVER);
+			
+				Connection connec =DriverManager.getConnection(JDBC_URL);
+				if (connec!=null)
+				{
+					System.out.println("connexion au serveur de Bd reuissi");
+					return connec;
+				}
+		} catch (SQLException e )
+		{
+			System.out.println("echec de connexion");
+		}
+		catch (ClassNotFoundException e) 
+		{
+	        System.out.println(e.getMessage());
+	        System.out.println("ERREUR : charger derby.jdbc.EmbeddedDriver ");
+	       
+	    }
+		return null;
+	}
+
+}
